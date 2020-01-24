2d9e670ad6119a67cbf4d978b5839b84f38ea0ae
otetLopez <otet.tud@gmail.com>
2020-01-23 23:15:38 -0500

    fix bug on creating a new note, added but commented out audio support

--- Notes/DetailViewController.swift
+++ Notes/DetailViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import MapKit
+import AVFoundation
 
-class DetailViewController: UIViewController, CLLocationManagerDelegate {
+class DetailViewController: UIViewController, CLLocationManagerDelegate, AVAudioRecorderDelegate {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
@@ -29,6 +30,11 @@ class DetailViewController: UIViewController, CLLocationManagerDelegate {
     var address : String?
     var mod : Bool?
     var mapViewMode : Bool?
+    
+    // For the audio
+    var recordButton: UIButton!
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
     
     func configureView() {
         // Update the user interface for the detail item.
@@ -68,10 +74,29 @@ class DetailViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         
         oldNote = detailItem
-        var realOldNote = Note(title: oldNote!.getTitle(), date: oldNote!.getDate(), info: oldNote!.getAddress())
-        temporaryNote = realOldNote
-        
+        if oldNote != nil {
+            var realOldNote = Note(title: oldNote!.getTitle(), date: oldNote!.getDate(), info: oldNote!.getAddress())
+            temporaryNote = realOldNote
+        }
         configureView()
+        
+        // Setup Audio recorder
+//        recordingSession = AVAudioSession.sharedInstance()
+//        do {
+//            try recordingSession.setCategory(.playAndRecord, mode: .default)
+//            try recordingSession.setActive(true)
+//            recordingSession.requestRecordPermission() { [unowned self] allowed in
+//                DispatchQueue.main.async {
+//                    if allowed {
+//                        self.loadRecordingUI()
+//                    } else {
+//                        // failed to record!
+//                    }
+//                }
+//            }
+//        } catch {
+//            // failed to record!
+//        }
     }
 
     var detailItem: Note? {
@@ -194,5 +219,65 @@ class DetailViewController: UIViewController, CLLocationManagerDelegate {
                    mapDelegate.delegate = self
                }
     }
+//
+//    func loadRecordingUI() {
+//        recordButton = UIButton(frame: CGRect(x: 64, y: 64, width: 128, height: 64))
+//        recordButton.setTitle("Tap to Record", for: .normal)
+//        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+//        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+//        view.addSubview(recordButton)
+//    }
+//
+//    func startRecording() {
+//        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+//
+//        let settings = [
+//            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+//            AVSampleRateKey: 12000,
+//            AVNumberOfChannelsKey: 1,
+//            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+//        ]
+//
+//        do {
+//            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+//            audioRecorder.delegate = self
+//            audioRecorder.record()
+//
+//            recordButton.setTitle("Tap to Stop", for: .normal)
+//        } catch {
+//            finishRecording(success: false)
+//        }
+//    }
+//
+//    func getDocumentsDirectory() -> URL {
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//        return paths[0]
+//    }
+//
+//    func finishRecording(success: Bool) {
+//        audioRecorder.stop()
+//        audioRecorder = nil
+//
+//        if success {
+//            recordButton.setTitle("Tap to Re-record", for: .normal)
+//        } else {
+//            recordButton.setTitle("Tap to Record", for: .normal)
+//            // recording failed :(
+//        }
+//    }
+//
+//    @objc func recordTapped() {
+//        if audioRecorder == nil {
+//            startRecording()
+//        } else {
+//            finishRecording(success: true)
+//        }
+//    }
+//
+//    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+//        if !flag {
+//            finishRecording(success: false)
+//        }
+//    }
 }
 
