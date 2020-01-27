114ca60ce5855e82cd68b7bc2011bfb60f96b7ab
otetLopez <otet.tud@gmail.com>
2020-01-26 23:12:29 -0500

    Clean up

--- Notes/DetailViewController.swift
+++ Notes/DetailViewController.swift
@@ -562,19 +562,5 @@ class DetailViewController: UIViewController, CLLocationManagerDelegate, AVAudio
             return filepath
         }
     }
-    
-//    func saveImage(imageView: ImageView) {
-//       //create an instance of the FileManager
-//       let fileManager = FileManager.default
-//       //get the image path
-//       let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
-//       //get the image we took with camera
-//       let image = imageView.image!
-//       //get the PNG data for this image
-//       let data = UIImagePNGRepresentation(image)
-//       //store it in the document directory    fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
-//    }
-    
-    
 }
 
